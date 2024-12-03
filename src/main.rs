2396fc48b6063c7f209874f3666072324e2d3b49
Wojtek <pulchnymamut@gmail.com>
2024-12-03 14:26:06 +0100

    setup problem 3

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@
 
 mod problem01;
 mod problem02;
+mod problem03;
 
 fn main() -> Result<(), ()> {
     //problem01::problem01()?;
-    problem02::problem02()?;
+    //problem02::problem02()?;
+    problem03::problem03()?;
     Ok(())
 }
---
+++ src/problem03.rs
@@ -0,0 +1,3 @@
+pub fn problem03() -> Result<(), ()> {
+    Ok(())
+}
