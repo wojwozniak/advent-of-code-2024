c3e84e42b796164949a573f61a148d8136bc3315
Wojtek <pulchnymamut@gmail.com>
2024-12-04 16:27:49 +0100

    setup day 4

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,12 @@
 mod problem01;
 mod problem02;
 mod problem03;
+mod problem04;
 
 fn main() -> Result<(), ()> {
     //problem01::problem01()?;
     //problem02::problem02()?;
-    problem03::problem03()?;
+    //problem03::problem03()?;
+    problem04::problem04()?;
     Ok(())
 }
---
+++ src/problem04.rs
@@ -0,0 +1,16 @@
+//use std::fs::File;
+//use std::io::BufReader;
+
+/*
+   Problem 03 of 2024 Advent of Code
+   https://adventofcode.com/2024/day/4
+*/
+pub fn problem04() -> Result<i64, ()> {
+    //let path = "../inputs/input04.txt";
+
+    // Open the file
+    //let file: File = File::open(&path).expect("Failed to open file");
+    //let reader: BufReader<File> = BufReader::new(file);
+
+    Ok(0)
+}
