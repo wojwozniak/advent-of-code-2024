e7575c5b1d7afb2aac8675a4136ca81497a9d2a9
Wojtek <pulchnymamut@gmail.com>
2024-12-05 18:33:48 +0100

    setup task 5

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,13 @@ mod problem01;
 mod problem02;
 mod problem03;
 mod problem04;
+mod problem05;
 
 fn main() -> Result<(), ()> {
     //problem01::problem01()?;
     //problem02::problem02()?;
     //problem03::problem03()?;
-    problem04::problem04()?;
+    //problem04::problem04()?;
+    problem05::problem05()?;
     Ok(())
 }
