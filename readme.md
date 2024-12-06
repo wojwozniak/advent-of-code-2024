ee5c44c38f64fe9804dad3ef86359ca8152a71b7
Wojtek <pulchnymamut@gmail.com>
2024-12-06 17:14:31 +0100

    day 6 finished

--- readme.md
+++ readme.md
@@ -11,3 +11,4 @@ This repository contains my solutions to the Advent of Code 2024 challenges, wri
 | 03  | [Problem 3](https://adventofcode.com/2024/day/3) | [Solution](src/problem03.rs) |
 | 04  | [Problem 4](https://adventofcode.com/2024/day/4) | [Solution](src/problem04.rs) |
 | 05  | [Problem 5](https://adventofcode.com/2024/day/5) | [Solution](src/problem05.rs) |
+| 06  | [Problem 6](https://adventofcode.com/2024/day/6) | [Solution](src/problem06.rs) |
--- src/problem06.rs
+++ src/problem06.rs
@@ -5,24 +5,107 @@ use std::io::{BufRead, BufReader};
    Problem 06 of 2024 Advent of Code
    https://adventofcode.com/2024/day/6
 */
-pub fn problem06() -> Result<i64, String> {
+pub fn problem06() -> Result<(i64, i64), String> {
     let path = "../inputs/input06.txt";
     let (data, _) = get_data(path)?;
 
+    // Find starting point and it's direction on grid
     let (start_r, start_c, start_dir) = find_start(&data);
-    let visited_count = calculate_visited_count(&data, start_r, start_c, start_dir);
 
-    println!("{}", visited_count);
-    Ok(visited_count)
+    // Create table where we will store visited point
+    let rows = data.len();
+    let cols = data[0].len();
+    let mut visited = vec![vec![' '; cols]; rows];
+
+    let visited_count = calculate_visited_count(&data, &mut visited, start_r, start_c, start_dir);
+    let amount_of_creatable_loops =
+        calculate_creatable_loops(&data, &mut visited, start_r, start_c, start_dir);
+
+    println!("{}, {}", visited_count, amount_of_creatable_loops);
+    Ok((visited_count, amount_of_creatable_loops))
+}
+
+/// Main task of second part -> reworked first, calling first with rotated dir
+/// on each step
+fn calculate_creatable_loops(
+    data: &Vec<Vec<char>>,
+    visited: &mut Vec<Vec<char>>,
+    start_r: usize,
+    start_c: usize,
+    start_dir: char,
+) -> i64 {
+    let rows = data.len() as i32;
+    let cols = data[0].len() as i32;
+    let mut visited = visited.clone();
+
+    let mut r = start_r as i32;
+    let mut c = start_c as i32;
+    let mut delta = dir_to_delta(start_dir);
+
+    let mut possible_loops: i64 = 0;
+
+    loop {
+        // Changed part
+        let mut modified_data = data.clone();
+        let target_r = r + delta.0;
+        let target_c = c + delta.1;
+
+        if target_r >= 0 && target_r < rows && target_c >= 0 && target_c < cols {
+            modified_data[target_r as usize][target_c as usize] = '#';
+        }
+
+        let rotated_delta = rotate_delta(delta);
+        let rotated_dir = delta_to_dir(rotated_delta);
+        if calculate_visited_count(&modified_data, &visited, start_r, start_c, rotated_dir) == -1 {
+            possible_loops += 1;
+        }
+        // EO changed part
+
+        let curr_r = r as usize;
+        let curr_c = c as usize;
+        let curr_dir = delta_to_dir(delta);
+
+        // Check if this cell was visited from the same direction
+        if visited[curr_r][curr_c] == curr_dir {
+            return -1;
+        }
+
+        // Mark cell as visited with current direction
+        if visited[curr_r][curr_c] == ' ' {
+            visited[curr_r][curr_c] = curr_dir;
+        }
+
+        let next_r = r + delta.0;
+        let next_c = c + delta.1;
+
+        // Check if next position is out of bounds, end loop
+        if next_r < 0 || next_r >= rows || next_c < 0 || next_c >= cols {
+            break;
+        }
+
+        // Check if next cell is blocked
+        if data[next_r as usize][next_c as usize] == '#' {
+            delta = rotate_delta(delta);
+            continue;
+        }
+
+        // Move to next position
+        r = next_r;
+        c = next_c;
+    }
+
+    possible_loops
 }
 
 /// Main task of first part -> calculating amount of visited fields
 fn calculate_visited_count(
     data: &Vec<Vec<char>>,
+    visited: &Vec<Vec<char>>,
     start_r: usize,
     start_c: usize,
     start_dir: char,
 ) -> i64 {
+    let mut visited = visited.clone();
     let rows = data.len() as i32;
     let cols = data[0].len() as i32;
 
@@ -31,14 +114,20 @@ fn calculate_visited_count(
     let mut delta = dir_to_delta(start_dir);
 
     let mut visited_count: i64 = 0;
-    let mut visited = vec![vec![false; cols as usize]; rows as usize];
 
     loop {
         let curr_r = r as usize;
         let curr_c = c as usize;
+        let curr_dir = delta_to_dir(delta);
 
-        if !visited[curr_r][curr_c] {
-            visited[curr_r][curr_c] = true;
+        // Check if this cell was visited from the same direction
+        if visited[curr_r][curr_c] == curr_dir {
+            return -1;
+        }
+
+        // Mark cell as visited with current direction
+        if visited[curr_r][curr_c] == ' ' {
+            visited[curr_r][curr_c] = curr_dir;
             visited_count += 1;
         }
 
@@ -96,6 +185,17 @@ fn dir_to_delta(dir: char) -> (i32, i32) {
         'v' => (1, 0),  // down
         '<' => (0, -1), // left
         _ => (0, 0),    // invalid
+    }
+}
+
+/// Change delta to dir to store visited for task 2
+fn delta_to_dir(delta: (i32, i32)) -> char {
+    match delta {
+        (-1, 0) => '^', // up
+        (0, 1) => '>',  // right
+        (1, 0) => 'v',  // down
+        (0, -1) => '<', // left
+        _ => ' ',       // invalid
     }
 }
 
