31a4202a39471cde2f6ee3d0e7147d4a1af1789e
Wojtek <pulchnymamut@gmail.com>
2024-12-15 16:30:45 +0100

    finish day 11

--- readme.md
+++ readme.md
@@ -4,13 +4,16 @@ This repository contains my solutions to the Advent of Code 2024 challenges, wri
 
 ## Solutions
 
-| Day | Problem                                          | Solution                     |
-| --- | ------------------------------------------------ | ---------------------------- |
-| 01  | [Problem 1](https://adventofcode.com/2024/day/1) | [Solution](src/problem01.rs) |
-| 02  | [Problem 2](https://adventofcode.com/2024/day/2) | [Solution](src/problem02.rs) |
-| 03  | [Problem 3](https://adventofcode.com/2024/day/3) | [Solution](src/problem03.rs) |
-| 04  | [Problem 4](https://adventofcode.com/2024/day/4) | [Solution](src/problem04.rs) |
-| 05  | [Problem 5](https://adventofcode.com/2024/day/5) | [Solution](src/problem05.rs) |
-| 06  | [Problem 6](https://adventofcode.com/2024/day/6) | [Solution](src/problem06.rs) |
-| 07  | [Problem 7](https://adventofcode.com/2024/day/7) | [Solution](src/problem07.rs) |
-| 08  | [Problem 8](https://adventofcode.com/2024/day/8) | [Solution](src/problem08.rs) |
+| Day | Problem                                            | Solution                                    |
+| --- | -------------------------------------------------- | ------------------------------------------- |
+| 01  | [Problem 01](https://adventofcode.com/2024/day/1)  | [Solution](src/problem01.rs)                |
+| 02  | [Problem 02](https://adventofcode.com/2024/day/2)  | [Solution](src/problem02.rs)                |
+| 03  | [Problem 03](https://adventofcode.com/2024/day/3)  | [Solution](src/problem03.rs)                |
+| 04  | [Problem 04](https://adventofcode.com/2024/day/4)  | [Solution](src/problem04.rs)                |
+| 05  | [Problem 05](https://adventofcode.com/2024/day/5)  | [Solution](src/problem05.rs)                |
+| 06  | [Problem 06](https://adventofcode.com/2024/day/6)  | [Solution](src/problem06.rs)                |
+| 07  | [Problem 07](https://adventofcode.com/2024/day/7)  | [Solution](src/problem07.rs)                |
+| 08  | [Problem 08](https://adventofcode.com/2024/day/8)  | [Solution](src/problem08.rs)                |
+| 09  | [Problem 09](https://adventofcode.com/2024/day/9)  | [Solution (first part)](src/problem09.rs)   |
+| 10  | [Problem 10](https://adventofcode.com/2024/day/10) | [Solution (not yet here)](src/problem10.rs) |
+| 11  | [Problem 11](https://adventofcode.com/2024/day/11) | [Solution](src/problem11.rs)                |
--- src/problem11.rs
+++ src/problem11.rs
@@ -1,9 +1,17 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
 
 /*
    Problem 11 of 2024 Advent of Code
    https://adventofcode.com/2024/day/11
+
+   First part -> just replace 75 with 25
+   The main idea here is that we use a HashMap instead of an array. Order
+   does not really matter, and using HashMap (pair of number, occurences) speeds
+   up the function massively (not having to do 236 trillion operations in last loop
+   is quite helpful). HashMap is best here because we still do a lot of operations
+   so we take advantage of O(1) read/write
 */
 pub fn problem11() -> Result<i64, ()> {
     let path = "../inputs/input11.txt";
@@ -22,43 +30,40 @@ pub fn problem11() -> Result<i64, ()> {
             .collect();
     }
 
-    let output = n_times_runner(numbers, 5);
+    let output = n_times_runner(numbers, 75);
 
     println!("{}", output);
     Ok(output as i64)
 }
 
-/// Function to run step of main n timess
-fn n_times_runner(mut numbers: Vec<i64>, n: i32) -> i64 {
+/// Function to run step of main n times -> reworked so it uses
+/// a hashmap instead of vector -> this way we speed it up massively
+fn n_times_runner(numbers: Vec<i64>, n: i32) -> i64 {
+    let mut counts: HashMap<i64, i64> = HashMap::new();
+
+    for num in numbers.iter() {
+        *counts.entry(*num).or_insert(0) += 1;
+    }
+
     for _ in 0..n {
-        //println!("{:?}", numbers);
-        numbers = process_numbers(numbers);
-        println!("{:?}", numbers);
-    }
-    numbers.len() as i64
-}
+        let mut new_counts: HashMap<i64, i64> = HashMap::new();
 
-/// Single step of first part of task
-fn process_numbers(mut numbers: Vec<i64>) -> Vec<i64> {
-    let mut i = 0;
-    while i < numbers.len() {
-        if numbers[i] == 0 {
-            numbers[i] = 1;
-            i += 1;
-        } else if has_even_digits(numbers[i]) {
-            let (first, second) = split_number(numbers[i]);
-
-            numbers.remove(i);
-            numbers.insert(i, second);
-            numbers.insert(i, first);
-
-            i += 2;
-        } else {
-            numbers[i] *= 2024;
-            i += 1;
+        for (&key, &value) in counts.iter() {
+            if key == 0 {
+                *new_counts.entry(1).or_insert(0) += value;
+            } else if has_even_digits(key) {
+                let (key1, key2) = split_number(key);
+                *new_counts.entry(key1).or_insert(0) += value;
+                *new_counts.entry(key2).or_insert(0) += value;
+            } else {
+                *new_counts.entry(key * 2024).or_insert(0) += value;
+            }
         }
+
+        counts = new_counts;
     }
-    numbers
+
+    counts.values().sum()
 }
 
 /// Auxilliary spliting function
@@ -80,4 +85,25 @@ fn split_number(num: i64) -> (i64, i64) {
 fn has_even_digits(num: i64) -> bool {
     let digit_count = (num.abs() as f64).log10().floor() as u32 + 1;
     digit_count % 2 == 0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_probem11_01() {
+        let numbers = vec![125, 17];
+        let result = n_times_runner(numbers, 6);
+
+        assert_eq!(result, 22);
+    }
+
+    #[test]
+    fn test_probem11_02() {
+        let numbers = vec![125, 17];
+        let result = n_times_runner(numbers, 25);
+
+        assert_eq!(result, 55312);
+    }
 }
